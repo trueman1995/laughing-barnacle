5231db9f3d6f33b22a471c80b62338465a17cd28
armbruster <armbruster@nerthus.aicas.burg>
2017-11-16 16:02:42 +0100

    added doku

--- JAM-4584_update_headers.pl
+++ JAM-4584_update_headers.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use File::Find;
 
+#take absolute path to directory as commandline argument or throw error and quit'
 my $dir = shift or die "Usage: $0 ABSOLUTE_PATH_TO_DIRECTORY\n";
 
 find(\&do, $dir);
@@ -14,12 +15,13 @@ sub do
     if ($file =~ /.*\.(java|c)$/){
 
         my $year = (localtime)[5] + 1900;
+        #open file and file.tmp or throw error and quit
         open (current_file, $file) or die "Error opening $file";
-        open (tmp_file,">","$file.tmp") or die "Error opening $file";
+        open (tmp_file,">","$file.tmp") or die "Error opening $file.tmp";
         print "$file\n";
 
         while (<current_file>){
-
+            #parse the file and replace the dates of copyright headers
             my $line =$_;
             if ($line =~ /Copyright.*?aicas GmbH/){
                 $line =~ /(19\d\d|20\d\d)/;
@@ -32,6 +34,7 @@ sub do
                 print tmp_file $line;
             }
         }
+        #close files rename file.tmp to file (replacing original file with tmp file)
         close tmp_file;
         close current_file;
         rename("$file.tmp", $file);
