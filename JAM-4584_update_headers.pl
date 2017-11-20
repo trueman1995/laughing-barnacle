cd11aa05b7456456955353c57611fc35d347141d
armbruster <armbruster@nerthus.aicas.burg>
2017-11-20 09:50:38 +0100

    final commit. tested sufficiently, refined enough.

--- JAM-4584_update_headers.pl
+++ JAM-4584_update_headers.pl
@@ -5,9 +5,12 @@ use warnings;
 use File::Find;
 
 #take absolute path to directory as commandline argument or throw error and quit'
-my $dir = shift or die "Usage: $0 ABSOLUTE_PATH_TO_DIRECTORY\n";
+my $dir = shift or die "\[CRHeader\] Usage: $0 ABSOLUTE_PATH_TO_DIRECTORY\n";
+my $counter = 0;
 
 find(\&do, $dir);
+
+print "\[CRHeader\] updated $counter files";
 
 sub do
 {
@@ -16,9 +19,8 @@ sub do
 
         my $year = (localtime)[5] + 1900;
         #open file and file.tmp or throw error and quit
-        open (current_file, $file) or die "Error opening $file";
-        open (tmp_file,">","$file.tmp") or die "Error opening $file.tmp";
-        print "$file\n";
+        open (current_file, $file) or die "\[CRHeader\] Error opening $file";
+        open (tmp_file,">","$file.tmp") or die "\[CRHeader\] Error opening $file.tmp";
 
         while (<current_file>){
             #parse the file and replace the dates of copyright headers
@@ -27,14 +29,20 @@ sub do
                 $line =~ /(19\d\d|20\d\d)/;
                 if ($1 && $1 eq $year){
                     print tmp_file " * Copyright $year, aicas GmbH; all rights reserved.\n";
-                }else {
+                }elsif(!$1) {
+                    print "\[CRHeader\] updating $file\n";
+                    $counter++;
+                    print tmp_file " * Copyright $year, aicas GmbH; all rights reserved.\n";
+                }else{
+                    print "\[CRHeader\] updating $file\n";
+                    $counter++;
                     print tmp_file " * Copyright $1-$year, aicas GmbH; all rights reserved.\n";
                 }
             }else {
                 print tmp_file $line;
             }
         }
-        #close files rename file.tmp to file (replacing original file with tmp file)
+        #close files, rename file.tmp to file (replacing original file with tmp file)
         close tmp_file;
         close current_file;
         rename("$file.tmp", $file);
